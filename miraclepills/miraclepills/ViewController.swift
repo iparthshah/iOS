e9f85b3e8484f3d339e2739bdcb389d20a62f2ab
iparthshah <parthshah@Parths-Mac-mini.local>
2017-04-12 22:36:34 -0700

    Miracle pills

---
+++ miraclepills/miraclepills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  miraclepills
+//
+//  Created by Parth shah on 4/6/17.
+//  Copyright © 2017 Parth shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
