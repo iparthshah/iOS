3e61c475add34642825ce759556b566b9c6d9e11
iparthshah <parthshah@Parths-Mac-mini.local>
2017-04-17 22:33:45 -0700

    Stack view

---
+++ stackViewProj/stackViewProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackViewProj
+//
+//  Created by Parth shah on 4/17/17.
+//  Copyright © 2017 Parth shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
